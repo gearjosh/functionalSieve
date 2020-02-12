cd53d4ed0a06d48c1b39727b17b817f3bebce5c7
Staff <staff@Josh.local>
2020-02-12 15:45:19 -0800

    clean up and refactor

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const primeSifter = (num) => {
   if (isNaN(num)) {
-    return
+    return 'Try a number, please!'
   } else {
-    const recursor = (pValue, ary) => {
-      if (pValue >= num) {
-        console.log(`in the home stretch! primes are ${ary}`)
-        return [...ary]
+    const recursor = (a, p = 2) => {
+      if (p >= num) {
+        return a
       } else {
-        const filterOutMultiples = n => n === pValue || n % pValue !== 0
-        const newPrimes = ary.filter(filterOutMultiples)
-        return recursor(pValue + 1, newPrimes)
+        const filterOutMultiples = n => n === p || n % p !== 0
+        const newPrimes = a.filter(filterOutMultiples)
+        return recursor(newPrimes, p + 1)
       }
     }
     let primes = [...Array(num+1).keys()].slice(2)
-    return recursor(2, primes)
+    return recursor(primes)
   }
 }
 
@@ -26,14 +25,14 @@ $(document).ready(function() {
 })
 
 // BELOW is my scratch pad for currying this: the next step
-// const recursor = (pValue, ary) => {
-//   if (pValue >= num) {
-//     // console.log(`in the home stretch! primes are ${ary}`)
-//     return [...ary]
+// const recursor = (p, a) => {
+//   if (p >= num) {
+//     // console.log(`in the home stretch! primes are ${a}`)
+//     return [...a]
 //   } else {
-//     newPrimes = ary.filter(n => n !== pValue && n % pValue !== 0)
-//     // console.log(`in the else at ${pValue}. newPrimes are: ${newPrimes}`)
-//     return recursor(pValue + 1, newPrimes)
+//     newPrimes = a.filter(n => n !== p && n % p !== 0)
+//     // console.log(`in the else at ${p}. newPrimes are: ${newPrimes}`)
+//     return recursor(p + 1, newPrimes)
 //   }
 // }
 // let primes = [...Array(num+1).keys()].slice(2)

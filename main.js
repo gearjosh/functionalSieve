f63940df4b55203ce040d15c94c3ee2bd8cb2785
Staff <staff@Josh.local>
2020-02-12 13:17:35 -0800

    add base logic

--- main.js
+++ main.js
@@ -1,4 +1,27 @@
+const primeSifter = (num) => {
+  if (isNaN(num)) {
+    return
+  } else {
+    const primes = new Array(num)
 
+    // refactor this into a recursive function
+    for (let index = 0; index < num; index++) {
+      primes[index] = index
+    }
+    let p = 2
+
+    // refactor to filter
+    primes.forEach((number) => {
+      if (number % p === 0) {
+        primes.slice(p, p+1)
+      }
+    })
+    p++
+    // return a function that contains:
+    // - the new array
+    // - incremented p
+  }
+}
 
 $(document).ready(function() {
   $('#numberSelector').submit(function(e){
